ef0a0c5e6edf0fcdbe45126ef9440c18370babc1
Rizki24-star <kikingar.567@gmail.com>
2023-08-02 10:28:18 +0700

    README update and database file

--- db_guest_book.sql
+++ db_guest_book.sql
@@ -1,14 +1,31 @@
-# Guest_Book_App
-Techincal Test IT Web Platform Developer Intern MSIB Batch 5 - Aksesmu
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Agu 2023 pada 05.20
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
 
-Client : React JS
-Server : Node JS
-Database : MySQL
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-How to run the App :Q
-- create a database with the name "db_guest_book" on your phpmyadmin
 
-and you can import this query (also available on the project with file name db_guest_book.sql) : 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_guest_book`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `guest_entries`
+--
 
 CREATE TABLE `guest_entries` (
   `id` bigint(20) NOT NULL,
@@ -19,6 +36,10 @@ CREATE TABLE `guest_entries` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `guest_entries`
+--
+
 INSERT INTO `guest_entries` (`id`, `name`, `date_of_birth`, `email`, `id_card_number`, `created_at`) VALUES
 (21, 'User 1', '2023-08-10', 'user1@gmail.com', 123456543, '2023-08-02 10:10:36'),
 (22, 'User 2', '2023-08-02', 'user2@gmail.com', 654323456, '2023-08-02 10:11:03'),
@@ -26,12 +47,22 @@ INSERT INTO `guest_entries` (`id`, `name`, `date_of_birth`, `email`, `id_card_nu
 (24, 'User 4', '2005-01-03', 'user4@gmail.com', 12345676543, '2023-08-02 10:11:48'),
 (25, 'User 5', '1998-06-25', 'user5@gmail.com', 5432345654345, '2023-08-02 10:12:25');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indeks untuk tabel `guest_entries`
@@ -63,11 +94,6 @@ ALTER TABLE `users`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 COMMIT;
 
--- Running Clinet 
-- npm install
-- npm start 
-
-
--- Running Server : 
-- npm install
-- npm start (default port is 8800)
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
